3964eb0dcc12bce75e445f5aa9298d6f7d08ebbf
Ola Wiberg <olawiberg@gmail.com>
2013-09-17 20:13:45 -0700

    Split auth into a new file.

--- humanapi.py
+++ humanapi.py
@@ -179,44 +179,6 @@ class HumanAPI(object):
         return '<HumanAPI %s>' % self.accessToken
 
 
-
-
-
-HumanApiAuth = OAuth2Service(
-    client_id=settings.CLIENT_ID,
-    client_secret=settings.CLIENT_SECRET,
-    name='humanapi',
-    authorize_url='https://user.humanapi.co/oauth/authorize',
-    access_token_url='https://user.humanapi.co/oauth/token',
-    base_url='https://api.humanapi.co/v1/human/')
-
-
-# Wrapper functions for building authorize URL and session easily
-def get_authorize_url(redirect_uri):
-    return HumanApiAuth.get_authorize_url(
-        response_type='code',
-        redirect_uri=redirect_uri,
-    )
-
-def get_auth_session(code, scope='profile'):
-    # retrieve the authenticated session (response is a JSON string, so we need a custom decoder)
-    session = HumanApiAuth.get_auth_session(data={
-        'scope': scope,
-        'code': code,
-    }, decoder=json.loads)
-
-    return session
-
-def recreate_session(access_token):
-    return OAuth2Session(
-        HumanAPI.client_id,
-        HumanAPI.client_secret,
-        access_token,
-        HumanApiAuth
-    )
-
-
-
 class Profile(object):
     def __init__(self, master):
         self.master = master
